835c685e76cebddf7557eaa3c5bc17a32cac8f15
xodud1202 <xodud1202@naver.com>
2026-03-12 21:43:40 +0900

    pm2 설정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,16 @@ module.exports = {
     apps: [
         {
             name: "react-shop",
-            script: "npm",
+            // npm start 대신 next를 직접 실행
+            script: "./node_modules/next/dist/bin/next",
             args: "start",
+            // 메모리 제한을 현실적으로 조정 (또는 삭제)
+            max_memory_restart: "300M",
             autorestart: false,
-            max_memory_restart: "200M"
+            env: {
+                PORT: 3014,
+                NODE_ENV: "production"
+            }
         }
     ]
 }
